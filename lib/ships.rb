cd64e08aa398c50a13c974fc72eed928dbe25f97
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-07 17:04:16 -0600

    Added hit test and method

--- lib/ships.rb
+++ lib/ships.rb
@@ -1,8 +1,18 @@
 class Ship
+  attr_reader :name, :length
 
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
   end
 
+  def health
+    @health
+  end
+
+  def hit
+    @health -= 1
+
+  end
 end
