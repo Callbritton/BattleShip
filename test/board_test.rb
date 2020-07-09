ac52f936020fd7236a4fc153c8327400cf20ece8
Chris Allbritton <cla.inbox@gmail.com>
2020-07-09 09:43:31 -0400

    Add test_it_can_add_cells to confirm board can be expanded if needed

--- test/board_test.rb
+++ test/board_test.rb
@@ -16,12 +16,20 @@ class BoardTest < Minitest::Test
   def test_it_can_validate_coordinates
     #skip
     board = Board.new
-require "pry"; binding.pry
+
     assert_equal true, board.valid_coordinate?("A1")
     assert_equal true, board.valid_coordinate?("D4")
     assert_equal false, board.valid_coordinate?("A5")
     assert_equal false, board.valid_coordinate?("E1")
     assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  def test_it_can_add_cells
+    #skip
+    board = Board.new
+
+    board.cells["D5"] = Cell.new("D5")
+    assert_equal true, board.valid_coordinate?("D5")
   end
 
 end
